c2d64b8b4fd813589e8e728171ce05ad7e9786e0
vinodchelmeda <vinodchelmeda@gmail.com>
2023-12-15 00:13:02 +0530

    removed unused code

--- src/App.js
+++ src/App.js
@@ -15,11 +15,6 @@ function App() {
     <Router>
     <div>
   <Header></Header>
-      {/* <Routes>
-      <Route path="foundations" element={<Foundations />} />
-      <Route path="icons" element={<Icons />} />
-   </Routes> */}
-
 
    <Routes>
         <Route path="uRIMS-Library/foundations" element={<Foundations/>} />
